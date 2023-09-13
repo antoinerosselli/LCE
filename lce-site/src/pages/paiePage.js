ec8b93d9df7914c1bb33b9f805c5fb0800915e50
antoine <antoine@ubuntu.myguest.virtualbox.org>
2023-09-13 18:02:22 +0200

    V1

--- lce-site/src/pages/paiePage.js
+++ lce-site/src/pages/paiePage.js
@@ -48,18 +48,108 @@ function Paie() {
         reader.onload = (e) => {
           const fileContent = e.target.result;
           const workbook = XLSX.read(fileContent, { type: 'binary' });
-  
-          const sheetName = workbook.SheetNames[2];
+        
+          const sheetName = workbook.SheetNames[selectedMonth];
           const worksheet = workbook.Sheets[sheetName];
-  
+        
           const data = XLSX.utils.sheet_to_json(worksheet);
-  
+        
           const moisData = data.filter((item) => /MOIS/i.test(item.NOM));
-  
-          moisDataMerged[file.name] = moisData;
-  
-          if (moisData.length > 0) {
-            resolve({ fileName: file.name, data: moisData });
+          
+          const moisDataObj = moisData[0];
+          console.log(moisDataObj);
+          const moisDataFilteredObj = {};
+
+          for (const key of Object.keys(moisDataObj)) {
+            let newKey = key;
+            if (key === '__EMPTY_4') {
+              newKey = 'Heures_travaillées';
+            }
+            if (key === '__EMPTY_5') {
+              newKey = 'Heures_formation';
+            }
+            if (key === '__EMPTY_6') {
+              newKey = 'Heures_Visite_Médicale';
+            }
+            if (key === '__EMPTY_7') {
+              newKey = 'Heures_de_nuit';
+            }
+            if (key === '__EMPTY_8') {
+              newKey = 'Prime_DATR';
+            }
+            if (key === '__EMPTY_9') {
+              newKey = 'Prime_de_poste';
+            }
+            if (key === '__EMPTY_10') {
+              newKey = 'Prime_HEAUME/TEV';
+            }
+            if (key === '__EMPTY_11') {
+              newKey = 'Prime_Responsabilité';
+            }
+            if (key === '__EMPTY_12') {
+              newKey = 'Prime_Astreinte';
+            }
+            if (key === '__EMPTY_13') {
+              newKey = 'Prime_Impatriation';
+            }
+            if (key === '__EMPTY_14') {
+              newKey = 'Prime_Exceptionnelle';
+            }
+            if (key === '__EMPTY_15') {
+              newKey = 'Ticket_Resto';
+            }
+            if (key === '__EMPTY_19') {
+              newKey = 'Absence_Authorisé';
+            }
+            if (key === '__EMPTY_20') {
+              newKey = 'Absence_Non_Authorisé';
+            }
+            if (key === '__EMPTY_23') {
+              newKey = 'Heure_de_route_vehicule_service';
+            }
+            if (key === '__EMPTY_24') {
+              newKey = 'Indemnité_forfaitaire_frais de voyage';
+            }
+            if (key === '__EMPTY_25') {
+              newKey = 'Heure_de_route_vehicule_perso';
+            }
+            if (key === '__EMPTY_26') {
+              newKey = 'Maintien_de_chambre';
+            }
+            if (key === '__EMPTY_28') {
+              newKey = '?';
+            }
+            if (key === '__EMPTY_29') {
+              newKey = 'Nombre_de_jours_GD';
+            }
+            if (key === '__EMPTY_30') {
+              newKey = 'Nombre_de_jours_RD';
+            }
+            if (key === '__EMPTY_31') {
+              newKey = 'PD Z1';
+            }
+            if (key === '__EMPTY_32') {
+              newKey = 'PD Z2';
+            }
+            if (key === '__EMPTY_33') {
+              newKey = 'PD Z3';
+            }
+            if (key === '__EMPTY_34') {
+              newKey = 'PD Z4';
+            }
+            if (key === '__EMPTY_35') {
+              newKey = 'PD Z5';
+            }
+            if (key.includes('EMPTY')) {
+              moisDataFilteredObj[newKey] = moisDataObj[key];
+            }
+          }        
+          console.log(moisDataFilteredObj);
+        
+          moisDataMerged[file.name] = moisDataFilteredObj;
+        
+          if (Object.keys(moisDataFilteredObj).length > 0) {
+            resolve({ fileName: file.name, data: moisDataFilteredObj });
           } else {
             resolve(null);
           }
@@ -102,18 +192,18 @@ function Paie() {
         <label>Choisir un mois:</label>
         <select value={selectedMonth} onChange={handleMonthSelect}>
           <option value=''>Sélectionnez un mois</option>
-          <option value='JANVIER'>Janvier</option>
-          <option value='FEVRIER'>Février</option>
-          <option value='MARS'>Mars</option>
-          <option value='AVRIL'>Avril</option>
-          <option value='MAI'>Mai</option>
-          <option value='JUIN'>Juin</option>
-          <option value='JUILLET'>Juillet</option>
-          <option value='AOUT'>Aout</option>
-          <option value='SEPTEMBRE'>Septembre</option>
-          <option value='OCTOBRE'>Octobre</option>
-          <option value='NOVEMBRE'>Novembre</option>
-          <option value='DECEMBRE'>Decembre</option>
+          <option value='1'>Janvier</option>
+          <option value='2'>Février</option>
+          <option value='3'>Mars</option>
+          <option value='4'>Avril</option>
+          <option value='5'>Mai</option>
+          <option value='6'>Juin</option>
+          <option value='7'>Juillet</option>
+          <option value='8'>Aout</option>
+          <option value='9'>Septembre</option>
+          <option value='10'>Octobre</option>
+          <option value='11'>Novembre</option>
+          <option value='12'>Decembre</option>
         </select>
       </div>
 
